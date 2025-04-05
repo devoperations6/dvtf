d30d992bd74a7f55f7b70281d72c4d72b0474812
devoperations6 <devoperations6@example.com>
2025-04-05 21:30:23 +0530

    updated both files

--- project_casa/modules/main.tf
+++ project_casa/modules/main.tf
@@ -1,10 +1,8 @@
-# main.tf
-
 module "vpc" {
   source = "./modules/vpc"
 
-  cidr_block     = "10.0.0.0/16"
-  vpc_name       = "casa_vpc"
+  cidr_block      = "10.0.0.0/16"
+  vpc_name        = "casa_vpc"
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnets  = ["10.0.3.0/24", "10.0.4.0/24"]
   azs             = ["us-east-1a", "us-east-1b"]
@@ -12,4 +10,22 @@ module "vpc" {
 
 output "vpc_id" {
   value = module.vpc.vpc_id
+}
+
+resource "aws_s3_bucket" "project_casa" {
+  bucket = "project_casa"
+  acl    = "private"
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  tags = {
+    Name        = "project_casa"
+    Environment = "Production"
+  }
 }
