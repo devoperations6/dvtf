fe4742c3f2dc76b48572bf086a63f2513a5031c6
devoperations6 <devoperations6@example.com>
2025-04-05 21:19:05 +0530

    updated bucket to store the tfstate files

--- project_casa/provider.tf
+++ project_casa/provider.tf
@@ -9,5 +9,16 @@ terraform {
 
 # Configure the AWS Provider
 provider "aws" {
-  region = "us-east-1"
+  region = "us-east-2"
+}
+
+# Backend configuration for Terraform state file storage in S3
+terraform {
+  backend "s3" {
+    bucket = "project_casa"    # S3 bucket name for storing the state file
+    key    = "terraform.tfstate"             # Path within the S3 bucket (can be a folder, too)
+    region = "us-east-2"                     # AWS region where the bucket is located
+    encrypt = true                           # Enable encryption for the state file (recommended)
+    
+  }
 }
