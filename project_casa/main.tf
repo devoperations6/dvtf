cb3f4c6d35d382895cf5d4d18dc48d0892f1e585
devoperations6 <devoperations6@example.com>
2025-04-06 12:40:04 +0530

    removed unsupported argument

--- project_casa/main.tf
+++ project_casa/main.tf
@@ -6,7 +6,6 @@ module "vpc" {
   vpc_name        = "casa_vpc"
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnets  = ["10.0.3.0/24", "10.0.4.0/24"]
-  azs             = ["us-east-1a", "us-east-1b"]
 }
 
 output "vpc_id" {
