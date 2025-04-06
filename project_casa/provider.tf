cde7ae97d0fb64c2b784bee9c90f5412c6da6130
devoperations6 <devoperations6@example.com>
2025-04-06 11:02:03 +0530

    updated tf backend file

--- project_casa/provider.tf
+++ project_casa/provider.tf
@@ -12,3 +12,12 @@ terraform {
 provider "aws" {
   region = "us-east-2"
 }
+
+terraform {
+  backend "s3" {
+    bucket         = "project_casa"  # Replace with your S3 bucket name
+    key            = "terraform.tfstate"  # Path to the state file in the bucket
+    region         = "us-east-2"  # Replace with your bucket's region
+    encrypt        = true  # Enable server-side encryption
+  }
+}
