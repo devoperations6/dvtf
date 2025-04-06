8127b248d63749872a4a72b0d64d645a396f279d
devoperations6 <devoperations6@example.com>
2025-04-06 10:55:16 +0530

    corrected subnet values

--- project_casa/modules/vpc/outputs.tf
+++ project_casa/modules/vpc/outputs.tf
@@ -7,10 +7,10 @@ output "vpc_id" {
 
 output "public_subnet_ids" {
   description = "IDs of the public subnets"
-  value       = aws_subnet.public[*].id
+  value       = aws_subnet.public_subnet[*].id
 }
 
 output "private_subnet_ids" {
   description = "IDs of the private subnets"
-  value       = aws_subnet.private[*].id
+  value       = aws_subnet.private_subnet[*].id
 }
