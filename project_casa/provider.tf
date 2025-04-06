b0a3f2fb6687c8fae0884a451b6add71c4b4036b
devoperations6 <devoperations6@example.com>
2025-04-06 11:08:07 +0530

    modifed keys and tf state file

--- project_casa/provider.tf
+++ project_casa/provider.tf
@@ -13,11 +13,11 @@ provider "aws" {
   region = "us-east-2"
 }
 
-terraform {
-  backend "s3" {
-    bucket         = "project_casa"  # Replace with your S3 bucket name
-    key            = "terraform.tfstate"  # Path to the state file in the bucket
-    region         = "us-east-2"  # Replace with your bucket's region
-    encrypt        = true  # Enable server-side encryption
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket         = "project_casa"  # Replace with your S3 bucket name
+#     key            = "terraform.tfstate"  # Path to the state file in the bucket
+#     region         = "us-east-2"  # Replace with your bucket's region
+#     encrypt        = true  # Enable server-side encryption
+#   }
+# }
